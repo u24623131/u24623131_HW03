811ca6a603b284f2235c0fd9fa2a1744a0ea48e5
u24623131 <u24623131@tuks.co.za>
2025-11-03 01:25:55 +0200

    Add project files.

---
+++ u24623131_HW3/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace u24623131_HW3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult Maintain()
+        {
+
+            return View();
+        }
+
+        public ActionResult Report()
+        {
+
+            return View();
+        }
+        public ActionResult Customer()
+        {
+
+            return View();
+        }
+        public ActionResult Product()
+        {
+
+            return View();
+        }
+        public ActionResult Staff()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ u24623131_HW3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="u24623131_HW3.MvcApplication" Language="C#" %>
